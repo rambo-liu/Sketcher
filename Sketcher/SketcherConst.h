0f3736082a07cda2686a59870cab7da655069e81
Rambo Liu <rambo.liu@outlook.com>
2016-06-02 17:26:16 +0800

    finish the chapter 15 of VC++ 2010

---
+++ Sketcher/SketcherConst.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "stdafx.h"
+
+enum ElementType
+{
+    LINE,
+    RECTANGLE,
+    CIRCLE,
+    CURVE,
+};
+
+const COLORREF BLACK = RGB(0, 0, 0);
+const COLORREF RED = RGB(255, 0, 0);
+const COLORREF GREEN = RGB(0, 255, 0);
+const COLORREF BLUE = RGB(0, 0, 255);
